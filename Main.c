f4e17ba56e9cad737d17b9dc17a8b508b42d4fbc
Lloyd <lloyd7miller@gmail.com>
2017-12-04 19:26:28 -0500

    the main method now sets the TURN.txt file back to just having a 0

--- Main.c
+++ Main.c
@@ -1,11 +1,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void setTurn();
+
 int main(){
+    printf("\nWelcome to the program!\n");
+    setTurn();
+    
     int pid = fork();
-    if(pid != 0)
-        producer();
+    if(pid == -1)
+        printf("Error: could not execute fork");
+        exit(-1);
     if(pid == 0)
+        producer();
+    else
         consumer();
+    
     return 0;
+}
+
+void setTurn(){
+    FILE* f = fopen("TURN.txt","w+"); //creates a new blank file, erasing the old one
+    putc('0', f);
 }
--- Producer.c
+++ Producer.c
@@ -2,5 +2,12 @@
 #include <stdio.h>
 
 int producer(){
+    FILE* turn = fopen("TURN.txt","rt");
+    //need to keep track of how far we got into the file...
+    //--> nvm, we just shove the char into the DATA.txt file
+    char c = fgetc(turn);
+    while(c != '0'){
+        continue;
+    }
     
 }
