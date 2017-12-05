4416eb4f84fb617090391a63c0fd4304366d4afc
Lloyd <lloyd7miller@gmail.com>
2017-12-05 17:40:35 -0500

    don't know if it works...

--- Main.c
+++ Main.c
@@ -8,23 +8,37 @@ int consumer();
 
 int main(){
     printf("\nWelcome to the program!\n");
-    setTurn();
+    
+    int test = 0;
+    int* ptr = &test;
+    setTurn(ptr);
     printf("after setTurn\n");
+    
     int pid = fork();
     if(pid == -1){
         printf("Error: could not execute fork");
         exit(1);
     }
-    if(pid == 0)
+    if(pid == 0){
+        printf("Running the producer file\n");
         producer();
-    else
+    }
+    else{
         printf("Running the consumer file\n");
-        //consumer();
+        consumer();
+    }
     
     return 0;
 }
 
-void setTurn(){
-    FILE* f = fopen("TURN.txt","w"); //creates a new blank file, erasing the old one
-    putc('0', f);
+void setTurn(int* b){
+    if(*b==0){
+        FILE* f = fopen("TURN.txt","w+"); //creates a new blank file, erasing the old one
+        printf("\tsetTurn() ran once\n");
+        fprintf(f, "0");
+        fclose(f);
+        *b = 1;
+    }else{
+        printf("\t\tnahh son\n");
+    }
 }
--- Producer.c
+++ Producer.c
@@ -2,18 +2,33 @@
 #include <stdio.h>
 
 int producer(){
+    printf("\tI am the producer file\n");
+    
     FILE* turn = fopen("TURN.txt","r");
     //need to keep track of how far we got into the file...
     //--> nvm, we just shove the char into the DATA.txt file
     char c = fgetc(turn);
-    int i = 0;
+    int pos = 0;
+    FILE *dataFile = fopen("mydata.txt", "rt");
+
     while(c == '0'){
-        FILE* dataFile = fopen("mydata.txt","rt");
-        if(i == 5){
-            printf("Ran %d times\n", i);
-            return 0;
-        }
-        i++;
+        //grab a char from mydata.txt
+        fseek(dataFile, pos, SEEK_SET);
+        char k = getc(dataFile);
+
+        //take that char and put it the DATA.txt exchanger file
+        FILE* exData = fopen("DATA.txt","w");
+        putc(k, exData);
+        fclose(exData);
+
+        //increment the position that we are at in the file
+        pos++;
+
+        //make it ahmed's turn
+        FILE* turn = fopen("TURN.txt", "w");
+        putc("1", turn);
+        fclose(turn);
     }
+    fclose(dataFile);
     return 0;
 }
